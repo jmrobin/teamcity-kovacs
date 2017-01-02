8035d12d6c4200a1995f4a214a6f43361f98c13c
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-01-02 05:45:13 +0100

    Add Help page to Home Controller

--- DemoSolution/src/app/Demo.Web/Controllers/HomeController.cs
+++ DemoSolution/src/app/Demo.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using toto;
 
 namespace Demo.Web.Controllers
 {
@@ -12,6 +13,11 @@ namespace Demo.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View ( );
         }
     }
 }
